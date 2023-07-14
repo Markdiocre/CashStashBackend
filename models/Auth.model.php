b695729a6b132cc1ab6acadc7630b99718be09d8
Markdiocre <202010597@gordoncollege.edu.ph>
2023-07-14 19:31:43 +0800

    Added token verifier

--- models/Auth.model.php
+++ models/Auth.model.php
@@ -36,21 +36,29 @@ class Auth
         $username = $data->username;
         $password = $data->password;
         $sql = "CALL authLogin(?)";
-
         $stmt = $this->pdo->prepare($sql);
         try {
             $stmt->execute([$username]);
             if ($stmt->rowCount() > 0) {
                 $res = $stmt->fetchAll()[0];
-                if ($this->checkPassword($password, $res['fld_password'])){
-                    return array("msg"=>"Nyekis");
-                }else{
+                $stmt->closeCursor();
+                if ($this->checkPassword($password, $res['fld_password'])) {
+                    $sql = "CALL createToken(?,?)";
+                    $token = $this->tokenGen($res['fld_personal_id']);
+                    $stmt = $this->pdo->prepare($sql);
+                    if ($stmt->execute([$res['fld_personal_id'], $token["token"]])) {
+                        return $this->gm->response_payload($token, "success", "Successfully Logged in", 200);
+                    } else {
+                        return $this->gm->response_payload(null, "failed", "Failed to logged in", 403);
+                    }
+                } else {
                     return $this->gm->response_payload(null, "failed", "Username and password does not match", 400);
-
                 }
-            }else{
+            } else {
                 return $this->gm->response_payload(null, "failed", "Account doesn't exist", 404);
             }
+
+            $stmt->closeCursor();
         } catch (\PDOException $e) {
             echo $e->getMessage();
         }
@@ -64,9 +72,9 @@ class Auth
             $data->password = $this->encrypt_password($data->password);
             if ($stmt->execute([$data->fname, $data->lname, $data->email, $data->username, $data->password])) {
                 $status = $stmt->fetch();
-                if($status['@is_success'] == 1){
+                if ($status['@is_success'] == 1) {
                     return $this->gm->response_payload(null, "success", "Successfully registered!", 200);
-                }else if($status['@is_success'] == 0){
+                } else if ($status['@is_success'] == 0) {
                     return $this->gm->response_payload($status['is_success'], "failed", "Username or Email is already registered", 400);
                 }
             }
@@ -79,7 +87,7 @@ class Auth
     public function tokenGen($id)
     {
         $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
-        $payload = json_encode(['fld_personal_id' => $id]);
+        $payload = json_encode(['fld_personal_id' => $id, 'exp' => date("Y-m-d", strtotime('+7 days'))]);
         $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
         $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
         $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, SECRET_KEY, true);
@@ -88,14 +96,35 @@ class Auth
         return array("token" => $jwt);
     }
 
-    public function verifyToken($jwt)
+    public function verifyToken()
     {
-        $decoded = explode(".", $jwt);
-        $payload = json_decode(str_replace(['+', '/', '='], ['-', '_', ''], base64_decode($decoded[1])));
-        $signature = hash_hmac('sha256', $decoded[0] . "." . $decoded[1], SECRET_KEY, true);
-        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-        if ($base64UrlSignature === $decoded[2]) {
-            return array("payload" => $payload, "is_valid" => True);
+        $jwt = explode(' ', $_SERVER['HTTP_AUTHORIZATION']);
+        if ($jwt[0] != 'Bearer') {
+            return false;
+        } else {
+            $decoded = explode(".", $jwt[1]);
+            $payload = json_decode(str_replace(['+', '/', '='], ['-', '_', ''], base64_decode($decoded[1])));
+            $signature = hash_hmac('sha256', $decoded[0] . "." . $decoded[1], SECRET_KEY, true);
+            $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+            if ($base64UrlSignature === $decoded[2]) {
+                $sql = "CALL checkToken(?,?,?)";
+                $stmt = $this->pdo->prepare($sql);
+                // echo var_dump($jwt[1]);
+                try {
+                    if ($stmt->execute([ $jwt[1], $payload->fld_personal_id, $payload->exp])) {
+                        $res = $stmt->fetch();
+                        if ($res["token_count"] == 1) {
+                            return true;
+                        } else {
+                            return true;
+                        }
+                    }
+                } catch (\PDOException $e) {
+                    echo $e->getMessage();
+                }
+            } else {
+                return false;
+            }
         }
     }
 }
--- routes/Try.routes.php
+++ routes/Try.routes.php
@@ -1,6 +1,16 @@
 <?php
-
-    if($req[0] == 'try'){ echo json_encode($auth->tokenGen(2)); return;}
-    if($req[0]=='jwt'){ echo json_encode($auth->verifyToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGRfcGVyc29uYWxfaWQiOjJ9.MIio7hBHNKZ0lwFOwHZ33xVVj7L4xoPWm5Z6xP3aPew"));  return;}
+if ($auth->verifyToken()) {
+    if ($req[0] == 'try') {
+        echo json_encode($auth->tokenGen(2));
+        return;
+    }
+    if ($req[0] == 'jwt') {
+        echo json_encode($auth->verifyToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGRfcGVyc29uYWxfaWQiOjJ9.MIio7hBHNKZ0lwFOwHZ33xVVj7L4xoPWm5Z6xP3aPew"));
+        return;
+    }
+}else{
+    $gm->response_payload(null, "failed", "You are not Authorized. Please log in first", 403);
+}
+   
     // if($req[0] == 'tokenvalid'){ echo "bobo"; return;}
     // if($req[0]=='try'): echo "Tanga"; endif; return;
